bfc32af7af585eabb511acee8048c558bfa706ee
Olga Nelioubov <onelioubov@gmail.com>
2020-11-14 22:23:52 -0500

    Renamed BlogPostController.cs to its pluralized form and set up routes for it.

---
+++ src/Api/Controllers/BlogPostsController.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using blog.Shared;
+
+namespace blog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BlogPostsController: ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Post>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("");
+        }
+        
+        [Route("{id:int}", Name = nameof(GetById))]
+        [HttpGet]
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok("");
+        }
+
+        [Route("{slug:string}", Name = nameof(GetBySlug))]
+        [HttpGet]
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetBySlug()
+        {
+            return Ok("");
+        }
+        
+        [Route("{tag:string}", Name = nameof(GetByTag))]
+        [HttpGet]
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetByTag()
+        {
+            return Ok("");
+        }
+    }
+}
