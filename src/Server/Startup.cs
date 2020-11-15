ca5d1190515f420a285d9476a037a9d8ea8ef0fc
Olga Nelioubov <onelioubov@gmail.com>
2020-11-14 20:57:02 -0500

    Some more initial changes to startup.

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -25,6 +25,7 @@ namespace blog.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddSwaggerDocument();
             
             services.AddControllersWithViews();
