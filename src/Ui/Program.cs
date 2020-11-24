ecdec4d91476df6ae707b816b3753bb07026a5df
Olga Nelioubov <onelioubov@gmail.com>
2020-11-24 01:32:13 -0500

    Added a new component for rendering each Blog card on the Blog page. Added Markdown support.

--- src/Ui/Program.cs
+++ src/Ui/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using Markdig;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace blog.Ui
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new  MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
 
             await builder.Build().RunAsync();
         }
